0e41daeff8bacc63f23d1602ba01eaf4831b221b
Anas.Attia <attia.mohamedanas@esprit.tn>
2022-11-17 00:13:27 +0000

    comm 2

--- src/main/java/tn/esprit/rh/achat/services/ReglementServiceImpl.java
+++ src/main/java/tn/esprit/rh/achat/services/ReglementServiceImpl.java
@@ -34,15 +34,15 @@ public class ReglementServiceImpl implements IReglementService {
 		return reglement;
 	}
 
-	//@Override
-	//public List<Reglement> retrieveReglementByFacture(Long idFacture) {
-	//	List<Reglement> reglements= reglementRepository.retrieveReglementByFacture(idFacture);
-	//	return reglements;
+	@Override
+	public List<Reglement> retrieveReglementByFacture(Long idFacture) {
+		List<Reglement> reglements= reglementRepository.retrieveReglementByFacture(idFacture);
+		return reglements;
 		
 //		ou bien(Sans JPQL)
 //		Facture f= factureRepository.findById(idFacture).get();
 //		return (List<Reglement>) f.getReglements();
-//	}
+	}
 
 	@Override
 	public float getChiffreAffaireEntreDeuxDate(Date startDate, Date endDate) {
